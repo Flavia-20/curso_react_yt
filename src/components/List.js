5571b6f5a61136b8d0cf2b2ec5202273ea986493
Flavia-20 <flaviamonique38@gmail.com>
2024-01-16 16:24:56 -0300

    Aprendendo o que são fragments

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,28 @@
 import Item from './Item'
 
+
+/*Primeira forma que o professor mostrou o fragment 
+
+  function List() {
+  return (
+    <>/*o fragment é deixar as tags internas sem uma tag pai, para isso deixo só as setas */
+/*      <h1>Minha Lista de Marcas:</h1>
+      <ul>
+        <li>Intem 1</li>
+        <li>Intem 2</li>
+      </ul>
+    </>
+  )
+}
+
+export default List
+*/
 function List() {
   return (
     <div>
       <h1>Minha Lista de Marcas:</h1>
       <ul>
-        <Item marca="Ferrari" />
+        <Item marca="Ferrari" />{/*Aqui ta usando props */}
         <Item marca="Fiat" />
         <Item marca="Honda" />
       </ul>
